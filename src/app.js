634139d5419815914dc6ea2f7c4b40af860602f2
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-02 20:14:07 -0500

    [+] Node: Configura posible configuración de puerto en la nube
    Aclaración:
    Usando el parámetro 'process.env.PORT' accede a la variable de
    entorno del servicio en la nube para usar el puerto configurado
    por defecto en el mismo. En caso de no encontrarlo usará por
    defecto el puerto 3000.
    
    Acciones:
     - Condiciona el uso del puerto
     - Despliega mensaje al lanzar el servidor
    
    Resultados:
     - El servidor corre en el puerto indicado
     - La ruta no proporciona un contenido específico

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const   express = require( 'express' ),
         app = express();
 
 // Settings
-app .set( 'port', 3000 );
+app .set( 'port', process .env .PORT || 3000 );   // Considera Puerto para servicios en la nube (Si existe que lo use, si no que use por defecto el 3000)
 
 // Server
-app .listen( app .get( 'port' ) );
+app .listen( app .get( 'port' ), () => {
+    console .log( `Servidor en el puerto: ${ app .get( 'port' ) }` );
+});
