9c383cd474d384890fe70d3729cbffa923222962
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-03 19:13:15 -0500

    [+] Views: Formulario Agregar Nota

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,6 +3,9 @@ const   express = require( 'express' ),
         router = express .Router();
 
 // Creamos las rutas 
+router .get( '/notes/add', ( request, response ) => {
+    response .render( 'notes/new-note' );
+}); 
 router .get( '/notes', ( request, response ) => {
     response .render( 'notes/notes' );
 });        
