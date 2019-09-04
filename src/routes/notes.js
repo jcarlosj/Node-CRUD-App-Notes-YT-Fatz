84f5573311953962e7454a030da1b3861dd224cd
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-03 19:17:15 -0500

    [+] Route(POST): Datos enviados por el formulario

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -6,6 +6,11 @@ const   express = require( 'express' ),
 router .get( '/notes/add', ( request, response ) => {
     response .render( 'notes/new-note' );
 }); 
+router .post( '/notes/new-note', ( request, response ) => {     // Recibe datos enviados por el formulario
+    console .log( request .body );
+    response .send( 'Los datos han sido enviados exitosamente por el método POST' );
+});
+
 router .get( '/notes', ( request, response ) => {
     response .render( 'notes/notes' );
 });        
