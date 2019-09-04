1ecfd86006811a3fefce7bde6cf611eef16ee024
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-03 19:42:40 -0500

    [+] Route(POST): Maneja Errores del Formulario

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -7,8 +7,24 @@ router .get( '/notes/add', ( request, response ) => {
     response .render( 'notes/new-note' );
 }); 
 router .post( '/notes/new-note', ( request, response ) => {     // Recibe datos enviados por el formulario
-    console .log( request .body );
-    response .send( 'Los datos han sido enviados exitosamente por el método POST' );
+    const { title, description } = request .body,               // Destructuring
+            errors = [];
+
+    // Validación de los campos
+    if( !title ) errors .push({ text: 'Por favor escriba un título' });
+    if( !description ) errors .push({ text: 'Por favor escriba una descripción' });
+
+    if( errors .length > 0 ) {
+        response .render( 'notes/new-note', {                  // Retorna el Formulario
+            errors,                                             // Pasa los mensajes de error a la vista
+            title,                                              // Pasa el título contenido a la vista
+            description                                         // Pasa la descripción a la vista
+        });                  
+    }
+    else {
+        response .send( 'Los datos han sido enviados exitosamente por el método POST' );
+    }
+    
 });
 
 router .get( '/notes', ( request, response ) => {
